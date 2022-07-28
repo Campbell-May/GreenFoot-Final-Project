f6e4666a1c9ed0d919fee5411ef7b37b716c0565
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-07-28 16:54:19 +1200

    Add auth logic to welcome page buttons

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthenticationButton from "./authentication-button";
-import Profile from "./Profile";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import AuthenticationButton from './AuthenticationButton'
+import Profile from './Profile'
 
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react'
 
-function Nav() {
-  const { isAuthenticated } = useAuth0();
+function Nav () {
+  const { isAuthenticated } = useAuth0()
 
   return (
     <nav className="navbar">
       <div className="navbar-items">
         <Link to="/">Welcome</Link>
-        <Link to="/home">Home</Link>
+        {isAuthenticated && <Link to="/home">Home</Link>}
         {isAuthenticated && <Link to="/mychallenges">My Challenges</Link>}
         <Link to="/tips">Tips & Tricks</Link>
       </div>
@@ -25,7 +25,7 @@ function Nav() {
         </div>
       </div>
     </nav>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,33 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import SignupButton from './SignupButton'
+import AuthenticationButton from './AuthenticationButton'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const handleClick = () => {
   window.location.href = '/home'
 }
 
-
 export const Welcome = () => {
+  const { isAuthenticated } = useAuth0()
+
+  const getButtons = () => {
+    if (isAuthenticated) {
+      return <button className="welcome-button" onClick={handleClick}>
+        <AuthenticationButton />
+      </button>
+    }
+    return <div className="button-row">
+      <button className="welcome-button" onClick={handleClick}>
+        <SignupButton />
+      </button>
+      <button className="welcome-button" onClick={handleClick}>
+        <AuthenticationButton />
+      </button>
+    </div>
+  }
+
   return (
-      <div className="animated-title">
-        <div className="one">
-          <span>Welcome</span>
-          <span>to</span><br />
-        </div>
-        <div className="two">
-          <img src="/transparent-green-foot.png" alt="Green Foot logo" />
-        </div>
-        <div className="three">
-          <span className="animated-content">Daily</span>
-          <span className="animated-content">challenges</span>
-          <span className="animated-content">to</span>
-          <span className="animated-content">help</span>
-          <span className="animated-content">save</span>
-          <span className="animated-content">the</span>
-          <span className="animated-content">planet</span>
-        </div>
-          <button id="bwelc" onClick={handleClick}>
-            Get Started
-          </button>
+    <div className="animated-title">
+      <div className="one">
+        <span>Welcome</span>
+        <span>to</span><br />
       </div>
-  );
-};
+      <div className="two">
+        <img src="/transparent-green-foot.png" alt="Green Foot logo" />
+      </div>
+      <div className="three">
+        <span className="animated-content">Daily</span>
+        <span className="animated-content">challenges</span>
+        <span className="animated-content">to</span>
+        <span className="animated-content">help</span>
+        <span className="animated-content">save</span>
+        <span className="animated-content">the</span>
+        <span className="animated-content">planet</span>
+      </div>
+      {getButtons()}
+    </div>
+  )
+}
